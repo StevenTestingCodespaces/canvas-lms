7ec26fe8d26731b871e819bbab1f51b02d05d0bc
Steven Nuñez <octosteve@github.com>
2023-06-21 00:23:30 +0000

    Start Redis on start

--- build/finalize-app-setup.sh
+++ build/finalize-app-setup.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-sudo update-rc.d redis-server enable
-sudo service redis-server start
-sudo service redis-server status
-
 ruby -rerb -e 'puts ERB.new(File.read(".devcontainer/domain.yml.erb")).result(binding)' > config/domain.yml
 cp config/database.yml.codespaces config/database.yml
 cp config/security.yml.example config/security.yml
 cp .devcontainer/cache_store.yml config/cache_store.yml
 cp .devcontainer/redis.yml config/redis.yml
 cp .devcontainer/dynamic_settings.yml config/dynamic_settings.yml
+
 bundle install
 yarn install
 
